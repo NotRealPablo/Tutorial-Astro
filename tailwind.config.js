6c22cb1071a0f4666a8649c4fca02b616a9ac690
nrpqp <pablo.quirilaop@gmail.com>
2025-07-02 01:16:10 -0400

    Header y Footer

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
