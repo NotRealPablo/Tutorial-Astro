bb5df927ca13c5a55fd1f6cb1559c382b1a40e30
nrpqp <pablo.quirilaop@gmail.com>
2025-07-03 22:57:25 -0400

    Teminando about y agregando proyectos

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import preact from "@astrojs/preact";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pqp.netlify.app/",
-  integrations: [preact()],
+  integrations: [preact(), react()],
 
   vite: {
     plugins: [tailwindcss()]
